55d37d86c672a4d78a801411fb4b83030c2fc4d3
monkeygq <hgq6389465@gmail.com>
2017-07-26 03:17:01 +0800

    dev/add: perf_event_type

--- readperf/perffile/records.h
+++ readperf/perffile/records.h
@@ -13,55 +13,63 @@
 #ifdef	__cplusplus
 extern "C" {
 #endif
-    
-    
+
+
 #include    <stdio.h>
 #include    "../util/origperf.h"
 #include    "../util/tree.h"
-    
+
 #define RECORD_TREE_LINK  ptl
-    
+
     struct record_t {
         TREE_ENTRY(record_t)      RECORD_TREE_LINK;
-	u32 pid, tid;
-	u32 cpu;
+        u32 pid, tid;
+        u32 cpu;
         u64 time;
         u64 nr;
-	u64 id;
+        u64 id;
         enum perf_event_type type;
+        /*
+         * PERF_RECORD_MMAP 1
+         * PERF_RECORD_LOST 2
+         * PERF_RECORD_COMM 3
+         * PERF_RECORD_EXIT 4
+         * PERF_RECORD_FORK 7
+         * PERF_RECORD_SAMPLE 9
+         */
     };
-    
+
     struct record_mmap {
         struct record_t header;
-	u64 start;
-	u64 len;
-	u64 pgoff;
-	char filename[PATH_MAX];
+        u64 start;
+        u64 len;
+        u64 pgoff;
+        char filename[PATH_MAX];
     };
-    
+
     struct record_comm {
         struct record_t header;
-	char comm[16];
+        char comm[16];
     };
-    
+
     struct record_fork {
         struct record_t header;
-	u32 ppid;
-	u32 ptid;
+        u32 ppid;
+        u32 ptid;
     };
-    
+
     struct record_sample {
         struct record_t header;
-	u64 ip;
-	u64 period;
+        u64 ip;
+        u64 period;
     };
-    
+
     typedef TREE_HEAD(_OrderTree, record_t) record_order_tree_t;
-    
+
     record_order_tree_t init_record_order();
     bool add_record_order( record_order_tree_t *tree, struct record_t *rec );
     void iterate_order( record_order_tree_t *tree, void (*callback)(struct record_t *proc, void *data), void *data );
-    
+
 #ifdef	__cplusplus
 }
 #endif
